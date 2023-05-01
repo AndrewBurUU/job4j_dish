c69a7e5a7083d5edf875213640b063a51aeb3091
AndrewBurUU <andriv80@gmail.com>
2023-05-01 15:36:14 +0800

    3. Монолитные системы [#338433]

---
+++ src/main/java/ru/job4j/dish/repository/DishRepository.java
@@ -0,0 +1,9 @@
+package ru.job4j.dish.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.job4j.dish.model.Dish;
+
+import java.util.*;
+
+public interface DishRepository extends CrudRepository<Dish, Integer> {
+}
