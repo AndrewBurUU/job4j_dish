7c7d395063e86393c747c4d90725a00bad1f0796
AndrewBurUU <andriv80@gmail.com>
2023-05-11 20:25:42 +0800

    4. Docker сервисы [#504781]

--- src/main/java/ru/job4j/dish/Main.java
+++ src/main/java/ru/job4j/dish/Main.java
@@ -8,7 +8,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+@SpringBootApplication
 public class Main extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class);
@@ -20,6 +26,33 @@ public class Main extends SpringBootServletInitializer {
         liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
         liquibase.setDataSource(ds);
         return liquibase;
+    }
+
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+        String value = System.getenv(sysEnv);
+        if (value == null) {
+            value = config.getProperty(key);
+        }
+        return value;
+    }
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = Main.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
     }
 
     public static void main(String[] args) {
