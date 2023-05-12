c4854588fd3500d8478ad2e607b9adf62a8d8e46
AndrewBurUU <andriv80@gmail.com>
2023-05-12 08:55:25 +0800

    4. Docker сервисы [#504781]_2

--- src/main/java/ru/job4j/dish/Main.java
+++ src/main/java/ru/job4j/dish/Main.java
@@ -28,33 +28,6 @@ public class Main extends SpringBootServletInitializer {
         return liquibase;
     }
 
-    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
-        String value = System.getenv(sysEnv);
-        if (value == null) {
-            value = config.getProperty(key);
-        }
-        return value;
-    }
-
-    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
-        var config = new Properties();
-        try (InputStream in = Main.class.getClassLoader()
-                .getResourceAsStream("application.properties")) {
-            config.load(in);
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
-        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
-        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
-        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
-        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
-        System.out.println("url=" + url);
-        Class.forName(driver);
-        return DriverManager.getConnection(
-                url, username, password
-        );
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
